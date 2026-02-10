7162005e8c5cf33243599fa754bbae632d195c6d
Yosmel Sanchez Caleo <43645777+kleo09800@users.noreply.github.com>
2026-02-10 12:57:33 -0500

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,7 +31,7 @@ export default function Navbar() {
     }`;
 
   return (
-    <nav className="fixed top-0 left-0 w-full h-16 z-50 bg-black/70 backdrop-blur border-b border-white/5">
+    <nav className="fixed top-0 left-0 w-full h-16 z-50 bg-transparent">
       <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
         {/* LOGO = HOME */}
         <a href="#home" className="flex items-center">
